b38e81f3c334ca9fec204bb56b033d7dc190ecb0
Esteban Espinoza <estebanespinoza146@gmail.com>
2020-04-11 02:49:40 +0000

    added form and am able to add players to database

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,4 +2,19 @@ class PlayersController < ApplicationController
   def index
     @player = Player.order("RANDOM()").first
   end
+
+  def new
+    @player = Player.new
+  end
+
+  def create
+    Player.create(player_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def player_params
+    params.require(:player).permit(:name, :team)
+  end
 end
